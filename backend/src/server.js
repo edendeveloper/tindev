21a5e6db27d02cff2c1f31102d5ec23cb435fc35
WELINGTON MANFRIN <welington.robertodev@gmail.com>
2021-02-17 08:35:28 -0300

    Criação do backend

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = requie('cors')
+const routes = require('./routes')
+const server = express()
+
+mongoose.connect('mongodb+srv://omnistack:9480huri@cluster0.mqqs8.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+server.use(cors())
+server.use(express.json())
+server.use(routes)
+server.listen(3333)
